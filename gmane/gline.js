a0de19d450994d51ebc65fb139398e2f00191524
HEXACTA\iguevara <iguevara@hexacta.com>
2019-02-15 13:24:56 -0300

    adding capstone last exercise

---
+++ gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','mac.com','cam.ac.uk','berkeley.edu','indiana.edu','uct.ac.za','unicon.net','gmail.com','ucdavis.edu','yale.edu'],
+['2005',57,12,7,12,12,14,6,10,11,5],
+['2006',533,200,205,195,185,132,136,123,116,116]
+];
